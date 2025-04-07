1f9721930c0c4059a578cc9a92739925d2472241
EthanPerello <115740777+EthanPerello@users.noreply.github.com>
2025-04-06 20:38:18 -0400

    Add Portals Unity project files

---
+++ Assets/Scripts/GameOverText.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameOverText : MonoBehaviour
+{
+
+    public static int gameOver = 0;
+    private Text gameOverText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameOverText = GetComponent<Text>();
+
+        gameOverText.color = new Color(0, 0, 0, 0);
+        gameOverText.text = "You Win!";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameOver == 1) {
+            gameOverText.color = new Color(1, 0, 0, 1);
+        }
+    }
+}
